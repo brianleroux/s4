ee0eb897b8299e273f2c6f5a3ddec61ab69ac73b
Brian LeRoux <brian@westcoastlogic.com>
2012-05-18 12:09:13 +0200

    and a param is not on this either; fml its early ok?

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ module.exports = {
     }
     ,
     urlFor: function(key) {
-        return 'https://s3.amazonaws.com/'+ this.config.bucket + '/' + this.config.folder + '/' + this.key
+        return 'https://s3.amazonaws.com/'+ this.config.bucket + '/' + this.config.folder + '/' + key
     }
     ,
     // save a jpg from the local filesystem and call cb(err, data)
